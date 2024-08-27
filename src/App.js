a6355866d79c83e55ccae27ed42cfb207d8792dd
Juan Esteban Mesa <practicante.si2@comware.com.co>
2024-08-27 14:10:01 -0500

    feat(IMPROVEMENTS): Calculadora con react.
    :construction: Se hace la estructura de la calculadora, se crea componente de boton con props children y se le crea funcion para obtener los operadores.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Boton from './componentes/Boton';
 
 function App() {
   return (
     <div className="App">
-      <div className='contenedor-calculadora'>
-        
+      <div className="contenedor-calculadora">
+        <div className="fila">
+          <Boton>1</Boton>
+        </div>
+        <div className="fila"></div>
+        <div className="fila"></div>
+        <div className="fila"></div>
+        <div className="fila"></div>
       </div>
     </div>
   );
